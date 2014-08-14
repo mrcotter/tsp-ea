143840762e9aabd827e41f5725ecf39213571c8d
mrcotter2013@gmail.com <mrcotter2013@gmail.com@db66126c-02a0-a6b8-2b7a-bb07df575e63>
2014-08-14 05:57:06 +0000

    continue on tsplib reader function, add classes "Individual", "Node"

---
+++ trunk/src/Node.java
@@ -0,0 +1,26 @@
+
+public class Node {
+
+    //The identifier of this node.
+    private final int id;
+
+    //The position of this node.
+    private final int[] position;
+
+    //Nodes constructor
+    public Node(int id, int[] position) {
+        super();
+        this.id = id;
+        this.position = position;
+    }
+
+    //Return the id of the no
+    public int getID() {
+        return id;
+    }
+
+    public int[] getPosition() {
+        return position;
+    }
+
+}

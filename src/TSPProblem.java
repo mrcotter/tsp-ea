c71eae44383aa14a4df71da266ca317afe98ae31
mrcotter2013@gmail.com <mrcotter2013@gmail.com@db66126c-02a0-a6b8-2b7a-bb07df575e63>
2014-08-14 05:57:06 +0000

    continue on tsplib reader function, add classes "Individual", "Node"

--- src/TSPProblem.java
+++ src/TSPProblem.java
@@ -1,24 +1,72 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class TSPProblem {
 
-    private Population tour;
+    private String problem_name;
 
-    //Constructs a new, empty TSPLIB problem
+    private String problem_comment;
+
+    private String problem_type;
+
+    private Individual tour;
+
+    //Constructs an empty TSPLIB problem
     public TSPProblem() {
         super();
-        tour = new Population();
+        tour = new Individual();
     }
 
     //Constructs a TSPLIB problem instance from a file
-    public TSPProblem(File file) throws IOException {
+    public TSPProblem(File file) {
         this();
         load(file);
     }
 
     //Load a problem from a TSPLIB file
-    public void load(File file) throws IOException {
+    private void load(File file) {
+        BufferedReader tsp_reader = null;
+        String line = null;
+
+        try {
+            tsp_reader = new BufferedReader(new FileReader(file));
+
+            while ((line = tsp_reader.readLine()) != null) {
+                line = line.trim();
+
+                if (line.isEmpty() || line.equals("NODE_COORD_SECTION")) {
+                    //Do nothing
+                } else if (line.equals("EOF")) {
+                    break;
+                }  else if (line.contains(":")) {
+                    String[] tokens = line.split(":");
+                    String token_0 = tokens[0].trim();
+                    String token_1 = tokens[1].trim();
+
+                    if (token_0.equals("NAME")) {
+                        problem_name = "Problem Name: " + token_1;
+                    }
+
+                    if (token_0.equals("COMMENT")) {
+                        problem_comment = "Comment: " + token_1;
+                    }
+
+                    if (token_0.equals("TYPE")) {
+                        problem_type = "Problem Type: " + token_1;
+                    }
+                } else {
+
+                }
+
+            }
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }

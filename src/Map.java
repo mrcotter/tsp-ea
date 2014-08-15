fc5d58592b12b98ae9363296c454d39a7516f315
mrcotter2013@gmail.com <mrcotter2013@gmail.com@db66126c-02a0-a6b8-2b7a-bb07df575e63>
2014-08-15 12:59:35 +0000

    Coding for Map, Node, TSPProblem, so it can read form a TSPLIB file

--- src/Map.java
+++ src/Map.java
@@ -1,8 +1,25 @@
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Map represents an area with given cities read from a TSPLIB file.
+ * It also provide a function that calculate the euclidean distances between cities.
  */
 
 public class Map {
+
+    //The nodes in the map
+    private final ArrayList<Integer> cities;
+
+    //Constructor of a map
+    public Map() {
+        super();
+        cities = new ArrayList<Integer>();
+    }
+
+    //Add the contents of this map from the TSPLIB reader
+    public void addNodes(Node node) throws IOException {
+        cities.add(node.getID());
+    }
 
 }

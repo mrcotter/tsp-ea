2cc2a8caef097afdd5f2176e434a17dd228aab2d
mrcotter2013@gmail.com <mrcotter2013@gmail.com@db66126c-02a0-a6b8-2b7a-bb07df575e63>
2014-08-15 13:08:59 +0000

    Exception handler modified

--- trunk/src/TSP_Driver.java
+++ trunk/src/TSP_Driver.java
@@ -3,7 +3,8 @@ import java.io.File;
 public class TSP_Driver {
     public static void main(String[] args) throws Exception {
 
-        TSPProblem problem = new TSPProblem(new File("../data/eil51.tsp"));
+        TSPProblem problem = new TSPProblem(new File("./data/eil51.tsp"));
+
 
     }
 }

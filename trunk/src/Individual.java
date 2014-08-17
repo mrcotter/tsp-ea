a7f91e0fd7a0cb96bf831a074c268674d34aaa62
zyksnowking@gmail.com <zyksnowking@gmail.com@db66126c-02a0-a6b8-2b7a-bb07df575e63>
2014-08-17 10:44:56 +0000

    Daniel(Population and Individual and Map)

--- trunk/src/Individual.java
+++ trunk/src/Individual.java
@@ -2,7 +2,80 @@
 /**
  * Individual represents a possible solution to the TSP as a permutation of the given cities.
  */
+import java.util.ArrayList;
+import java.util.Random;
+
+
 
 public class Individual {
+	
+	private ArrayList<Node> single_tour = new ArrayList<Node>();
+	
+	private Map map;
+	
+	public Individual(Map map)
+	{
+		this.map = map;
+	}
+	
+	//Create a random tour
+	public void CreateRandomTour()
+	{
+		for(int i=0; i<map.NumberOfNodes(); i++)
+		{
+			single_tour.set(i, map.GetNode(i));
+		}
+		
+		long seed = System.nanoTime();
+		
+		//Shuffle the the list
+		Random random = new Random(seed);
+		random.nextInt();
+		for (int i = 0; i < single_tour.size(); i++) 
+		{
+		    int shuffle_Num = i + random.nextInt(single_tour.size() - i);
+		    single_tour.set(i, single_tour.get(shuffle_Num));
+		    single_tour.set(shuffle_Num, single_tour.get(i));
+		}
+	}
+	
+	//Get a node from a single tour
+	public Node GetANode(int pos)
+	{
+		return single_tour.get(pos);
+	}
+	
+	//Get the number of the nodes in a tour
+	public int NumberOfNodes()
+	{
+		return single_tour.size();
+	}
+	
+	//Calculate the total distance of a single tour
+	public double TotalDistance()
+	{
+		double TotalDistance = 0;
+		Node Node_Start;
+		Node Node_End;
+		
+		for(int i = 0; i < single_tour.size(); i++)
+		{
+			Node_Start = GetANode(i);
+			
+			if(i+1 < single_tour.size())
+			{
+				Node_End = GetANode(i+1);
+			}
+			else
+			{
+				Node_End = GetANode(0);
+			}
+			
+			TotalDistance = TotalDistance + Node_Start.distanceTo(Node_End);
+		}
+		
+		
+		return TotalDistance;
+	}
 
 }

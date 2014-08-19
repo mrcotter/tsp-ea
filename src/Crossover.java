327748e781c801e2e034d07b5268e2ca72cbdcd6
zyksnowking@gmail.com <zyksnowking@gmail.com@db66126c-02a0-a6b8-2b7a-bb07df575e63>
2014-08-19 13:37:28 +0000

    Daniel Update 2 (Individual and Crossover)

--- src/Crossover.java
+++ src/Crossover.java
@@ -1,3 +1,4 @@
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
@@ -35,13 +36,79 @@ public class Crossover {
 		
 		if(RandomPos_1 > RandomPos_2)
 		{
-			Node[] temp = new Node[RandomPos_1 - RandomPos_2 + 1];
-			
+			for(int i = RandomPos_2; i <= RandomPos_1; i++)
+			{
+				Child.SetANode(i, Parent_1.GetANode(i));
+			}
 		}
+		
+		if(RandomPos_1 < RandomPos_2)
+		{
+			for(int i = RandomPos_1; i <= RandomPos_2; i++)
+			{
+				Child.SetANode(i, Parent_1.GetANode(i));
+			}
+		}
+		
+		for(int i = 0; i < Parent_2.NumberOfNodes(); i++)
+		{
+			
+			if(Child.IfContainANode(Parent_2.GetANode(i)) == false)
+			{
+				for(int j = 0; j < Child.NumberOfNodes(); j++)
+				{
+					if(Child.GetANode(j) == null)
+					{
+						Child.SetANode(j, Parent_2.GetANode(i));
+					}
+				}
+			}
+		}	
 		
 		
 		return Child;
 	}
+	
+	//PMX crossover
+	public Individual Crossover_PMX()
+	{
+		Individual Child = new Individual();
+		
+		int[] Random_Int = new int[Parent_1.NumberOfNodes()];
+		
+		for (int j = 0; j < Random_Int.length; j++)
+		{
+			Random_Int[j] = j;
+		}
+		
+		Collections.shuffle(Arrays.asList(Random_Int));
+		
+		int RandomPos_1 = Random_Int[0];
+		long seed = System.nanoTime();
+		Random rand = new Random(seed);
+		int RandomPos_2 = Random_Int[rand.nextInt(Parent_1.NumberOfNodes()-1) + 1];
+		
+		if(RandomPos_1 > RandomPos_2)
+		{
+			for(int i = RandomPos_2; i <= RandomPos_1; i++)
+			{
+				Child.SetANode(i, Parent_1.GetANode(i));
+			}
+		}
+		
+		if(RandomPos_1 < RandomPos_2)
+		{
+			for(int i = RandomPos_1; i <= RandomPos_2; i++)
+			{
+				Child.SetANode(i, Parent_1.GetANode(i));
+			}
+		}
+		
+		
+		
+		return Child;
+	}
+	
 
 }
 

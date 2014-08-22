ef2926e95263476019f771ff4afd1659cefa2ee1
mrcotter2013@gmail.com <mrcotter2013@gmail.com@db66126c-02a0-a6b8-2b7a-bb07df575e63>
2014-08-22 14:04:41 +0000

    Add PMX for crossover, still not working properly

--- trunk/src/Crossover.java
+++ trunk/src/Crossover.java
@@ -22,12 +22,10 @@ public class Crossover {
         //Choose two arbitrary parts for start and end from parents
         int number1 = rand.nextInt(size);
         int number2 = rand.nextInt(size);
-        //System.out.println(number1 + ", " + number2);
 
         int start = Math.min(number1, number2);
         int end = Math.max(number1, number2);
-
-        System.out.println("start: " + start + ", end: " + end);
+        //System.out.println("start: " + start + ", end: " + end);
 
         //Initialise children
         List<Node> child_1 = new Vector<Node>();
@@ -43,6 +41,7 @@ public class Crossover {
 
         for (int i = 0; i < size; i++) {
             current_index = (end + i) % size;
+            //System.out.print(current_index + " ");
             current_node_in_tour1 = tour_1.get(current_index);
             current_node_in_tour2 = tour_2.get(current_index);
 
@@ -61,11 +60,105 @@ public class Crossover {
         Collections.rotate(child_1, start);
         Collections.rotate(child_2, start);
 
-
+        /*
         //Output test for parents
+        System.out.println();
         System.out.println(parent_1.toString());
         System.out.println(parent_2.toString());
         System.out.println();
+
+        //Output test for children
+        for (int i = 0; i < size; i++) {
+            System.out.print(child_1.get(i).getID() + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < size; i++) {
+            System.out.print(child_2.get(i).getID() + " ");
+        }
+        */
+    }
+	
+	//PMX crossover
+	public void Crossover_PMX(Individual parent_1, Individual parent_2) {
+
+        int size = parent_1.NumberOfNodes();
+
+        Random rand = new Random();
+        //Choose two arbitrary parts for start and end from parents
+        int number1 = rand.nextInt(size);
+        int number2 = rand.nextInt(size);
+
+        int start = Math.min(number1, number2);
+        int end = Math.max(number1, number2);
+        System.out.println("start: " + start + ", end: " + end);
+
+        //Initialise children
+        List<Node> child_1 = parent_1.getNodeList();
+        List<Node> child_2 = parent_2.getNodeList();
+
+        //Get the parts in each tour
+        List<Node> section_1 = child_1.subList(start, end);
+        List<Node> section_2 = child_2.subList(start, end);
+
+        Node current_node, replace_node;
+        int replace_index;
+        int count = 0;
+
+        //Iterate each city which is not in the section
+        for (int i = end % size; i >= end || i < start; i = (i + 1) % size) {
+            count++;
+            //--------------- Child 1 ----------------
+            current_node = child_1.get(i);
+
+            //If that city is in the random part
+            if (section_1.contains(current_node)) {
+                //Get the index of the node to replace the repeated node within the section
+                replace_index = section_1.indexOf(current_node);
+
+                //Get the node that is intended to replace the repeated node
+                replace_node = section_2.get(replace_index);
+
+                //If the repeated node is also contained in the section
+                while (section_1.contains(replace_node)) {
+                    //Repeat the process again
+                    replace_index = section_1.indexOf(replace_node);
+                    replace_node = section_2.get(replace_index);
+                }
+
+                //Replace the current node with the replacement node
+                child_1.set(i, replace_node);
+            }
+
+            //--------------- Child 2 ----------------
+            current_node = child_2.get(i);
+
+            //If that city is in the random part
+            if (section_2.contains(current_node)) {
+                //Get the index of the node to replace the repeated node within the section
+                replace_index = section_2.indexOf(current_node);
+
+                //Get the node that is intended to replace the repeated node
+                replace_node = section_1.get(replace_index);
+
+                //If the repeated node is also contained in the section
+                while (section_2.contains(replace_node)) {
+                    //Repeat the process again
+                    replace_index = section_2.indexOf(replace_node);
+                    replace_node = section_1.get(replace_index);
+                }
+
+                //Replace the current node with the replacement node
+                child_2.set(i, replace_node);
+            }
+        }
+
+
+        //Output test for parents
+        System.out.println("count: " + count);
+        System.out.println(parent_1.toString());
+        System.out.println(parent_2.toString());
+        System.out.println();
+
         //Output test for children
         for (int i = 0; i < size; i++) {
             System.out.print(child_1.get(i).getID() + " ");
@@ -75,66 +168,9 @@ public class Crossover {
             System.out.print(child_2.get(i).getID() + " ");
         }
 
+
     }
-	
-	//PMX crossover
-	public Individual Crossover_PMX(Individual parent_1, Individual parent_2)
-	{
-		Individual Child = new Individual();
-		Individual SubSet = new Individual();
-		
-		int[] Random_Int = new int[parent_1.NumberOfNodes()];
-		
-		for (int j = 0; j < Random_Int.length; j++)
-		{
-			Random_Int[j] = j;
-		}
-		
-		Collections.shuffle(Arrays.asList(Random_Int));
-		
-		int RandomPos_1 = Random_Int[0];
-		long seed = System.nanoTime();
-		Random rand = new Random(seed);
-		int RandomPos_2 = Random_Int[rand.nextInt(parent_1.NumberOfNodes()-1) + 1];
-		
-		if(RandomPos_1 > RandomPos_2)
-		{
-			for(int i = RandomPos_2; i <= RandomPos_1; i++)
-			{
-				SubSet.SetANode(i, parent_1.GetANode(i));
-			}
-		}
-		
-		if(RandomPos_1 < RandomPos_2)
-		{
-			for(int i = RandomPos_1; i <= RandomPos_2; i++)
-			{
-				SubSet.SetANode(i, parent_1.GetANode(i));
-			}
-		}
-		
-		for(int i = 0; i < parent_2.NumberOfNodes(); i++)
-		{
-			for(int j = 0; j<Child.NumberOfNodes(); j++)
-			{
-				if(parent_2.GetANode(i) == SubSet.GetANode(j))
-				{
-					Child.SetANode(j, parent_2.GetANode(i));
-					Child.SetANode(i, parent_2.GetANode(j));
-				}
-			}
-		}
-		
-		for(int i = 0; i < Child.NumberOfNodes(); i++)
-		{
-			if(Child.GetANode(i) == null)
-			{
-				Child.SetANode(i, parent_2.GetANode(i));
-			}
-		}
-		
-		return Child;
-	}
+
 	
 
 }

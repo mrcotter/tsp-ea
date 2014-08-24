19a4f6150dc68ea90db1cde9ac1834c119b920fb
mrcotter2013@gmail.com <mrcotter2013@gmail.com@db66126c-02a0-a6b8-2b7a-bb07df575e63>
2014-08-24 04:58:42 +0000

    Add tournament selection method

--- src/Selection.java
+++ src/Selection.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Selection represents fitness based competition to select parents and offsprings
@@ -13,7 +14,7 @@ public class Selection {
     //Fitness-Proportionate Selection
     public Individual Selection_FPS(ArrayList<Individual> tours) {
 
-        getRawFitness(tours);
+        getRawFitness(tours);    //Move to GA later
 
         double sum_of_fitness = 0.0;
         double sum_of_probability = 0.0;
@@ -37,6 +38,24 @@ public class Selection {
         return null;
     }
 
+    //Tournament Selection
+    public Individual Selection_Tournament(ArrayList<Individual> tours, int tournament_size) {
+
+        ArrayList<Individual> tournament_pool = new ArrayList<Individual>(tournament_size);
+
+        for (int i = 0; i < tournament_size; i++) {
+            //Add random individual to the tournament pool
+            Random rand = new Random();
+            tournament_pool.add(tours.get(rand.nextInt(tours.size())));
+        }
+
+        //System.out.println(findShortest(tournament_pool).toString());
+
+        return findShortest(tournament_pool);
+    }
+
+
+    //Move to GA later
     //Calculate the fitness for all tours in given list containing tours
     private void getRawFitness(ArrayList<Individual> tours) {
         raw_fitness = new ArrayList<Double>(tours.size());
@@ -46,6 +65,21 @@ public class Selection {
             //System.out.println(tour.TotalDistance());
             raw_fitness.add(tour.TotalDistance());
         }
+    }
+
+    //Find out the shortest tour from the tournament pool or other collections of tours
+    public Individual findShortest(ArrayList<Individual> tours) {
+
+        Individual shortest = tours.get(0);
+
+        for(int i = 1; i < tours.size(); i++) {
+
+            if (shortest.TotalDistance() > tours.get(i).TotalDistance()) {
+                shortest = tours.get(i);
+            }
+        }
+
+        return shortest;
     }
 
 

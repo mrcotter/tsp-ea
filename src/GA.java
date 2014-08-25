fb3efe02b5b234ddc89ab49107b9b796bab165e3
mrcotter2013@gmail.com <mrcotter2013@gmail.com@db66126c-02a0-a6b8-2b7a-bb07df575e63>
2014-08-25 00:14:01 +0000

    Add functions to read from config file, then run GA

--- src/GA.java
+++ src/GA.java
@@ -5,4 +5,24 @@
 
 public class GA {
 
+    private int pop_size, generation, mut_type, cross_type, sel_type;
+    private double mut_rate, cross_rate;
+
+    public GA(int pop_size, int generations, double mut_rate,
+              int mut_type, double cross_rate, int cross_type, int sel_type) {
+
+        this.pop_size = pop_size;
+        this.generation =generations;
+        this.mut_rate = mut_rate;
+        this.mut_type = mut_type;
+        this.cross_rate = cross_rate;
+        this.cross_type = cross_type;
+        this.sel_type = sel_type;
+    }
+
+    public void runGA() {
+
+    }
+
+
 }
--- src/TSP_Driver.java
+++ src/TSP_Driver.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
@@ -6,18 +8,81 @@ public class TSP_Driver {
     public static void main(String[] args) throws Exception {
 
 
+        String file_name = "";
+        int pop_size = 0, generations = 0;
+        double mut_rate = 0.0, cross_rate = 0.0;
+        int mut_type = 0, cross_type = 0, sel_type = 0;
 
-        TSPProblem problem = new TSPProblem(new File("./data/eil10.tsp"));
+        //Read config file
+        BufferedReader config_reader = null;
+        String line;
+
+        try {
+            config_reader = new BufferedReader(new FileReader("./config.txt"));
+
+            while ((line = config_reader.readLine()) != null) {
+                line = line.trim();
+
+                if (!line.isEmpty() || !line.contains("#")) {
+                    if (line.contains("=")) {
+
+                        String[] tokens = line.split("=");
+                        String token_0 = tokens[0].trim();
+                        String token_1 = tokens[1].trim();
+
+                        if (token_0.equals("tsp")) {
+                            file_name = token_1;
+                        }
+
+                        if (token_0.equals("pop_size")) {
+                            pop_size = Integer.parseInt(token_1);
+                        }
+
+                        if (token_0.equals("generations")) {
+                            generations = Integer.parseInt(token_1);
+                        }
+
+                        if (token_0.equals("mut_rate")) {
+                            mut_rate = Double.parseDouble(token_1);
+                        }
+
+                        if (token_0.equals("cross_rate")) {
+                            cross_rate = Double.parseDouble(token_1);
+                        }
+
+                        if (token_0.equals("mut_type")) {
+                            mut_type = Integer.parseInt(token_1);
+                        }
+
+                        if (token_0.equals("cross_type")) {
+                            cross_type = Integer.parseInt(token_1);
+                        }
+
+                        if (token_0.equals("sel_type")) {
+                            sel_type = Integer.parseInt(token_1);
+                        }
+                    }
+                }
+            }
+        } finally {
+            if (config_reader != null) {
+                config_reader.close();
+            }
+        }
+
+        TSPProblem problem = new TSPProblem(new File("./data/" + file_name));
         //problem.printProblem();
 
         //Map map = problem.getMap();
         //map.printNodes();
 
-
         long start = System.currentTimeMillis();
 
-        String pop_size = null, generations;
-        double mut_rate, cross_rate;
+        //Start GA
+        GA ga = new GA(pop_size, generations, mut_rate, mut_type,
+                        cross_rate, cross_type, sel_type);
+        ga.runGA();
+
 
         //--------------------- Testing -------------------
         //Individual tour = new Individual(map);

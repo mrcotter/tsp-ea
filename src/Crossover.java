35297fbb36e64ed1e1d4a158164e51abcce0c08b
linhaijin@gmail.com <linhaijin@gmail.com@db66126c-02a0-a6b8-2b7a-bb07df575e63>
2014-08-26 13:38:39 +0000

    crossover

--- src/Crossover.java
+++ src/Crossover.java
@@ -12,77 +12,80 @@ public class Crossover {
 
     public Crossover() {}
 
-    //Order crossover
-    public void Crossover_Order(Individual parent_1, Individual parent_2) {
+  //Order crossover
+	public void Crossover_Order(Individual parent_1, Individual parent_2) {
 
-        List<Node> tour_1 = parent_1.getNodeList();
-        List<Node> tour_2 = parent_2.getNodeList();
+	        List<Node> tour_1 = parent_1.getNodeList();
+	        List<Node> tour_2 = parent_2.getNodeList();
 
-        int size = parent_1.NumberOfNodes();
+	        int size = parent_1.NumberOfNodes();
 
-        Random rand = new Random();
-        //Choose two arbitrary parts for start and end from parents
-        int number1 = rand.nextInt(size);
-        int number2 = rand.nextInt(size);
+	        Random rand = new Random();
+	        //Choose two arbitrary parts for start and end from parents
+	        int number1 = rand.nextInt(size);
+	        int number2 = rand.nextInt(size);
 
-        int start = Math.min(number1, number2);
-        int end = Math.max(number1, number2);
-        //System.out.println("start: " + start + ", end: " + end);
+	        int start = Math.min(number1, number2);
+	        int end = Math.max(number1, number2);
+	        //System.out.println("start: " + start + ", end: " + end);
 
-        //Initialise children
-        List<Node> child_1 = new Vector<Node>();
-        List<Node> child_2 = new Vector<Node>();
+	        //Initialise children
+	        List<Node> child_1 = new Vector<Node>();
+	        List<Node> child_2 = new Vector<Node>();
 
-        //Copy the part in between the start and end to the children
-        child_1.addAll(tour_1.subList(start, end));
-        child_2.addAll(tour_2.subList(start, end));
+	        //Copy the part in between the start and end to the children
+	        child_1.addAll(tour_1.subList(start, end));
+	        child_2.addAll(tour_2.subList(start, end));
 
-        //Do order procedure
-        int current_index;
-        Node current_node_in_tour1, current_node_in_tour2;
+	        //Do order procedure
+	        int current_index;
+	        Node current_node_in_tour1, current_node_in_tour2;
 
-        for (int i = 0; i < size; i++) {
-            current_index = (end + i) % size;
-            //System.out.print(current_index + " ");
-            current_node_in_tour1 = tour_1.get(current_index);
-            current_node_in_tour2 = tour_2.get(current_index);
+	        for (int i = 0; i < size; i++) {
+	            current_index = (end + i) % size;
+	            //System.out.print(current_index + " ");
+	            current_node_in_tour1 = tour_1.get(current_index);
+	            current_node_in_tour2 = tour_2.get(current_index);
 
-            //If child 1 does not contain the current node in tour 2, then add it
-            if (!child_1.contains(current_node_in_tour2)) {
-                child_1.add(current_node_in_tour2);
-            }
+	            //If child 1 does not contain the current node in tour 2, then add it
+	            if (!child_1.contains(current_node_in_tour2)) {
+	                child_1.add(current_node_in_tour2);
+	            }
 
-            //If child 2 does not contain the current node in tour 1, then add it
-            if (!child_2.contains(current_node_in_tour1)) {
-                child_2.add(current_node_in_tour1);
-            }
-        }
+	            //If child 2 does not contain the current node in tour 1, then add it
+	            if (!child_2.contains(current_node_in_tour1)) {
+	                child_2.add(current_node_in_tour1);
+	            }
+	        }
 
-        //Rotate the list so that the elements are in the right place
-        Collections.rotate(child_1, start);
-        Collections.rotate(child_2, start);
+	        //Rotate the list so that the elements are in the right place
+	        Collections.rotate(child_1, start);
+	        Collections.rotate(child_2, start);
 
-        //Add to the offspring list
-        offsprings.add(new Individual(new ArrayList<Node>(child_1)));
-        offsprings.add(new Individual(new ArrayList<Node>(child_2)));
+	        //Add to the offspring list
+	        offsprings.add(new Individual(new ArrayList<Node>(child_1)));
+	        offsprings.add(new Individual(new ArrayList<Node>(child_2)));
 
-        /*
-        //Output test for parents
-        System.out.println();
-        System.out.println(parent_1.toString());
-        System.out.println(parent_2.toString());
-        System.out.println();
+	     /*   
+	        //Output test for parents
+	        System.out.println();
+	        System.out.println(parent_1.toString());
+	        System.out.println(parent_2.toString());
+	        System.out.println();
 
-        //Output test for children
-        for (int i = 0; i < size; i++) {
-            System.out.print(child_1.get(i).getID() + " ");
-        }
-        System.out.println();
-        for (int i = 0; i < size; i++) {
-            System.out.print(child_2.get(i).getID() + " ");
-        }
-        */
-    }
+	        //Output test for children
+	        for (int i = 0; i < size; i++) {
+	            System.out.print(child_1.get(i).getID() + " ");
+	        }
+	        System.out.println();
+	        for (int i = 0; i < size; i++) {
+	            System.out.print(child_2.get(i).getID() + " ");
+	        }
+	        System.out.println();
+	        //System.out.println(offsprings.get(0).toString());
+	        //System.out.println(offsprings.get(1).toString());
+	        */
+	    }
 
     //PMX crossover
     public void Crossover_PMX(Individual parent_1, Individual parent_2) {
@@ -273,6 +276,22 @@ public class Crossover {
         
         //System.out.println(offsprings.get(0).toString());
         //System.out.println(offsprings.get(1).toString());
+/*
+        	        //Output test for parents
+	        System.out.println();
+	        System.out.println(parent_1.toString());
+	        System.out.println(parent_2.toString());
+	        System.out.println();
+
+	        //Output test for children
+	        for (int i = 0; i < size; i++) {
+	            System.out.print(child_1.get(i).getID() + " ");
+	        }
+	        System.out.println();
+	        for (int i = 0; i < size; i++) {
+	            System.out.print(child_2.get(i).getID() + " ");
+	        }
+	        */
     }
 
         // Edge Recombination Crossover
